a1580ef321fe630670182a68b594807df327f84d
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2025-03-18 20:02:05 +0100

    main: update running module with some argparse
    add options to generate class diagram by running module
    
    Signed-off-by: Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>

--- src/syrenka/__main__.py
+++ src/syrenka/__main__.py
@@ -1,21 +1,31 @@
+import sys
+import argparse
 import syrenka
-from syrenka.base import generate_class_list_from_module
-from syrenka.flowchart import SyrenkaFlowchart, FlowchartDirection
+from syrenka.base import classes_in_module
 
+def _class_diagram(args):
+    classes = classes_in_module(args.module, args.nested)
+
+    class_diagram = syrenka.SyrenkaClassDiagram()
+    class_diagram.add_classes(classes)
+
+    for line in class_diagram.to_code():
+        print(line)
+
+
+def _main():
+    ap = argparse.ArgumentParser(allow_abbrev=False)
+    
+    subparsers = ap.add_subparsers()
+    class_diagram = subparsers.add_parser('class', aliases=['c', 'classdiagram', 'class_diagram'])
+    class_diagram.add_argument("module")
+    class_diagram.add_argument("-n", "--nested", action='store_true')
+    class_diagram.set_defaults(func=_class_diagram)
+    
+    args = ap.parse_args()
+    return args.func(args)
 
 
 if __name__ == "__main__":
-    flowchart = SyrenkaFlowchart("", FlowchartDirection.LeftToRight)
-    for l in flowchart.to_code():
-        print(l)
-
-    class_list = generate_class_list_from_module("syrenka", "Mermaid")
-
-    mm = syrenka.SyrenkaClassDiagram()
-    
-    mm.add_classes(class_list)
-    #mm.add_class(str)
-    
-    r = mm.to_code()
-    for l in r:
-        print(l)
+    ret = _main()
+    sys.exit(ret)
