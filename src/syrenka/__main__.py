9fa76a24d99a31efd5b243c2683aa6a5069dddca
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2025-03-19 20:09:57 +0100

    main: fix when subcommand is not specified
    detect when subcommand is not specified
    and print usage
    
    also update prog name printed in help/usage
    
    Signed-off-by: Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>

--- src/syrenka/__main__.py
+++ src/syrenka/__main__.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import argparse
 import syrenka
@@ -15,9 +16,13 @@ def _class_diagram(args):
 
 
 def _main():
-    ap = argparse.ArgumentParser(allow_abbrev=False)
+    prog = os.path.basename(sys.argv[0])
+    if prog.endswith("__main__.py"):
+        prog = "python -m syrenka"
 
-    subparsers = ap.add_subparsers()
+    ap = argparse.ArgumentParser(prog=prog, allow_abbrev=False)
+
+    subparsers = ap.add_subparsers(dest="cmd")
     class_diagram = subparsers.add_parser(
         "class", aliases=["c", "classdiagram", "class_diagram"]
     )
@@ -26,6 +31,10 @@ def _main():
     class_diagram.set_defaults(func=_class_diagram)
 
     args = ap.parse_args()
+    if args.cmd is None:
+        ap.print_usage()
+        return -1
+
     return args.func(args)
 
 
