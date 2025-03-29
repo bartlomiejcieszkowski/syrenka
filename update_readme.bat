c2b58690fbde855b18f8dbe8341d3d559b78488c
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2025-03-29 16:15:35 +0100

    update_readme.bat update
    mmdc to generate svg
    
    Signed-off-by: Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>

--- update_readme.bat
+++ update_readme.bat
@@ -1,18 +1,20 @@
-echo ```mermaid> mermaid_diagram.txt
-uv run python examples\class_list_module.py >> mermaid_diagram.txt
-echo ```>> mermaid_diagram.txt
-python replace_between_tags.py -s "<!-- EX1_MERMAID_DIAGRAM_BEGIN -->" -e "<!-- EX1_MERMAID_DIAGRAM_END -->" -f README.md -r mermaid_diagram.txt
+echo ```python> mermaid_diagram.md
+type examples\class_list_module.py >> mermaid_diagram.md
+echo. >> mermaid_diagram.md
+echo ```>> mermaid_diagram.md
 
-echo ```python> mermaid_diagram.txt
-type examples\class_list_module.py >> mermaid_diagram.txt
-echo. >> mermaid_diagram.txt
-echo ```>> mermaid_diagram.txt
+python replace_between_tags.py -s "<!-- EX1_SYRENKA_CODE_BEGIN -->" -e "<!-- EX1_SYRENKA_CODE_END -->" -f README.md -r mermaid_diagram.md
 
-python replace_between_tags.py -s "<!-- EX1_SYRENKA_CODE_BEGIN -->" -e "<!-- EX1_SYRENKA_CODE_END -->" -f README.md -r mermaid_diagram.txt
+echo ```cmd> mermaid_diagram.md
+uv run python examples\class_list_module.py >> mermaid_diagram.md
+echo ```>> mermaid_diagram.md
+python replace_between_tags.py -s "<!-- EX1_MERMAID_DIAGRAM_RAW_BEGIN -->" -e "<!-- EX1_MERMAID_DIAGRAM_RAW_END -->" -f README.md -r mermaid_diagram.md
 
-echo ```cmd> mermaid_diagram.txt
-uv run python examples\class_list_module.py >> mermaid_diagram.txt
-echo ```>> mermaid_diagram.txt
-python replace_between_tags.py -s "<!-- EX1_MERMAID_DIAGRAM_RAW_BEGIN -->" -e "<!-- EX1_MERMAID_DIAGRAM_RAW_END -->" -f README.md -r mermaid_diagram.txt
+echo ```mermaid> mermaid_diagram.md
+uv run python examples\class_list_module.py >> mermaid_diagram.md
+echo ```>> mermaid_diagram.md
+python replace_between_tags.py -s "<!-- EX1_MERMAID_DIAGRAM_BEGIN -->" -e "<!-- EX1_MERMAID_DIAGRAM_END -->" -f README.md -r mermaid_diagram.md
 
-del mermaid_diagram.txt
+mmdc -i mermaid_diagram.md -o mermaid_diagram.svg
+
+del mermaid_diagram.md
