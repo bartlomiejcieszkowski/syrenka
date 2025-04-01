a4ac367eaafb70cfd00d32f06a3f707afbed0820
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2025-04-01 20:11:25 +0200

    update example to use config and theme
    Signed-off-by: Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>

--- examples/class_list_module.py
+++ examples/class_list_module.py
@@ -1,7 +1,10 @@
-import syrenka
+from syrenka.classdiagram import SyrenkaClassDiagram, SyrenkaClassDiagramConfig
+from syrenka.base import ThemeNames
 from syrenka.lang.python import PythonModuleAnalysis
 
-class_diagram = syrenka.SyrenkaClassDiagram("syrenka class diagram")
+class_diagram = SyrenkaClassDiagram(
+    "syrenka class diagram", SyrenkaClassDiagramConfig().theme(ThemeNames.forest)
+)
 class_diagram.add_classes(
     PythonModuleAnalysis.classes_in_module(module_name="syrenka", nested=True)
 )
